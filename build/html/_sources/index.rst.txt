0f37a03f91e39eeda7d266ec69477196012528bd
EgonSpengler <>
2023-11-17 09:53:54 +0100

    Initial Commit

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+
+========
+gameDocs
+========
+
+.. important::
+
+   This project is under active development.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: PC Games
+
+   starcraft <pcGames/starcraft.md>
+   steel division II <pcGames/steelDivision2.md>
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Sonstige
+
+   schach <sonstiges/schach.md>
---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'gameDocs'
+copyright = '2023, Olav Hautaklyttn'
+author = 'Olav Hautaklyttn'
+release = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ['_static']
+html_title = "gameDocs"
+
+html_theme_options = {
+    "sidebar_hide_name": False,
+}
